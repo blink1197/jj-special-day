fb2257905322948a384728c5bca6c4b26256f0a2
blink1197 <cgbagotsay11@gmail.com>
2025-11-16 05:55:14 +0800

    Import bootstrap packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+// bootstrap
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App)
+    .mount('#app')
